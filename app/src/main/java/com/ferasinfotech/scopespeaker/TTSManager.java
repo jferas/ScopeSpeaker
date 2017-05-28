2e0d53f3684c5f5fdbec6786b205fac05192a274
John Feras <jferas@ferasinfotech.com>
2017-05-28 02:17:28 -0400

    first experiments with switching voice

--- app/src/main/java/com/ferasinfotech/scopespeaker/TTSManager.java
+++ app/src/main/java/com/ferasinfotech/scopespeaker/TTSManager.java
@@ -8,10 +8,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
+import android.speech.tts.Voice;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 /**
  * Created by Nilanchala
@@ -24,6 +30,14 @@ public class TTSManager {
     private ScopeSpeakerActivity scopeSpeakerActivity = null;
     private Context the_context = null;
     private Bundle params = null;
+    private Set<Locale> mLocales = null;
+    private String mEngineName = null;
+    private Locale mDefaultLanguage = null;
+    private Voice mDefaultVoice = null;
+    private List<TextToSpeech.EngineInfo> mEngines = null;
+    private Locale mLanguage = null;
+    private Voice mVoice = null;
+    private Set<Voice> mVoices = null;
 
     public void init(Context context, ScopeSpeakerActivity ssa) {
         try {
@@ -43,6 +57,28 @@ public class TTSManager {
             if (status == TextToSpeech.SUCCESS) {
                 int result = mTts.setLanguage(Locale.US);
                 isLoaded = true;
+
+                mLocales = mTts.getAvailableLanguages();
+                mEngineName = mTts.getDefaultEngine();
+                mDefaultLanguage = mTts.getDefaultVoice().getLocale();
+                mDefaultVoice = mTts.getDefaultVoice();
+                mEngines = mTts.getEngines();
+                mLanguage = mTts.getVoice().getLocale();
+                mVoice = mTts.getVoice();
+                mVoices = mTts.getVoices();
+
+                Log.e("tts", "Available languages: " + mLocales);
+                Log.e("tts", "Default engine: " + mEngineName);
+                Log.e("tts", "Default language: " + mDefaultLanguage);
+                Log.e("tts", "Default voice: " + mVoice);
+                Log.e("tts", "Available engines: " + mEngines);
+                Log.e("tts", "Current language: " + mLanguage);
+                Log.e("tts", "Current voice: " + mVoice);
+                Log.e("tts", "Available voices: " + mVoices);
+
+                Set<Voice> englishVoices = getAvailableVoicesForLanguage("en");
+
+
 
                 if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                     Log.e("tts", "This Language is not supported");
@@ -100,5 +136,24 @@ public class TTSManager {
             Log.e("tts", "Text to be said was:" + text);
             Toast.makeText(the_context, "TTS Not Initialized", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public Set<Voice> getAvailableVoicesForLanguage(String language) {
+        Set<Voice> theVoices = new HashSet<Voice>();
+        Iterator<Voice> iterator = mVoices.iterator();
+        while (iterator.hasNext()) {
+            Voice theVoice = iterator.next();
+            if (theVoice.getName().indexOf(language + "-") == 0) {
+                Boolean isInstalled = !theVoice.getFeatures().contains("notInstalled");
+                if (isInstalled) {
+                    theVoices.add(theVoice);
+                    if (theVoice.getName().equals("en-us-x-sfg#female_3-local")) {
+                        mTts.setVoice(theVoice);
+                    }
+                }
+            }
+
+        }
+        return theVoices;
     }
 }
