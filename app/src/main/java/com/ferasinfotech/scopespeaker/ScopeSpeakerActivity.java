41b082a4481d066f59abf5786a8585b6dcf3797a
John Feras <jferas@ferasinfotech.com>
2017-08-01 01:50:13 -0400

    updated help to reflect skip message feature. Also, now source language doesn't effect display name decision

--- app/src/main/java/com/ferasinfotech/scopespeaker/ScopeSpeakerActivity.java
+++ app/src/main/java/com/ferasinfotech/scopespeaker/ScopeSpeakerActivity.java
@@ -543,7 +543,8 @@ public class ScopeSpeakerActivity extends AppCompatActivity implements WebSocket
                 + "While ScopeSpeaker is running in the background, it is continuously listening to the chat messages of the Periscope stream, saying them aloud and translating them if necessary.<br><br>"
                 + "ScopeSpeaker can also be run in split-screen mode as a companion app to Periscope.<br><br>"
                 + "Split screen mode allows ScopeSpeaker settings and preferences to be changed while broadcasting. ScopeSpeaker can also be run on a separate device for that purpose.<br><br>"
-                + "The 'Copy' button will cause the current chat messages to be copied to the Android clipboard.<br><br>"
+                + "The 'Copy To Clipboard' button will cause the current chat messages to be copied to the Android clipboard.<br><br>"
+                + "The 'Skip Message' button will immediately cause the current chat message to be aborted and the next message in the queue (if present) will be said.<br><br>"
                 + "The 'Change Voice' menu option allows ScopeSpeaker to use any of the voices installed in your Android device. You can install additional voices with a variety of accents via the Android 'Language and Input' / 'Text to Speech' settings.<br><br>"
                 + "<u>Preferences:</u><br><br>"
                 + "Slide the switches to enable or disable the announcements of users joining or leaving the chats.<br><br>"
@@ -972,7 +973,7 @@ public class ScopeSpeakerActivity extends AppCompatActivity implements WebSocket
                     String chat_message_language = languageArray.getString(0);
                     String display_name = sender.getString("display_name");
                     String user_name = sender.getString("username");
-                    if (saying_display_names && chat_message_language.equals("en")) {
+                    if (saying_display_names) {
                         who_said_it = display_name;
                     }
                     else {
